c9827ca90782c485a6cd04484ce1cb9efe3a32dc
Junhui Yang <119138211+yjhjaja@users.noreply.github.com>
2023-10-13 14:25:02 -0400

    Update get_centroid_of_polygon.py

--- get_centroid_of_polygon.py
+++ get_centroid_of_polygon.py
@@ -3,14 +3,15 @@ import arcpy
 def get_centroid_of_polygon(in_features, field_name_lat = 'lat', field_name_lon = 'lon', coordinate_system = arcpy.SpatialReference('WGS 1984')):
 	'''
 	Generate the centroid of each polygon in a polygon shapefile
-
-    Args:
-       in_features (string): directory of the shapefile
-       field_name_lat (string): name of the latitude  field of the centroid
-       field_name_lon (string): name of the longitude field of the centroid
-       coordinate_system (SpatialReference): spatial reference of the coordinates of the centroid
-    Returns:
-       nothing; simply edits in_features
+ 	
+  	Args:
+   		in_features (string): directory of the shapefile
+     		field_name_lat (string): name of the latitude  field of the centroid
+       		field_name_lon (string): name of the longitude field of the centroid
+	 	coordinate_system (SpatialReference): spatial reference of the coordinates of the centroid
+   	
+    	Returns:
+     		nothing; simply edits in_features
 	'''
 
 	arcpy.management.AddField(in_features, field_name_lat, 'DOUBLE')
