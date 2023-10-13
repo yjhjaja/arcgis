76a175378b867bf24469a1596aaa17194abbd00d
Junhui Yang <119138211+yjhjaja@users.noreply.github.com>
2023-10-13 14:23:36 -0400

    Add files via upload
    Given a polygon shapefile, this function returns the coordinates of the centroid of each polygon.

---
+++ get_centroid_of_polygon.py
@@ -0,0 +1,19 @@
+import arcpy
+
+def get_centroid_of_polygon(in_features, field_name_lat = 'lat', field_name_lon = 'lon', coordinate_system = arcpy.SpatialReference('WGS 1984')):
+	'''
+	Generate the centroid of each polygon in a polygon shapefile
+
+    Args:
+       in_features (string): directory of the shapefile
+       field_name_lat (string): name of the latitude  field of the centroid
+       field_name_lon (string): name of the longitude field of the centroid
+       coordinate_system (SpatialReference): spatial reference of the coordinates of the centroid
+    Returns:
+       nothing; simply edits in_features
+	'''
+
+	arcpy.management.AddField(in_features, field_name_lat, 'DOUBLE')
+	arcpy.management.AddField(in_features, field_name_lon, 'DOUBLE')
+
+	arcpy.management.CalculateGeometryAttributes(in_features, [[field_name_lat, 'CENTROID_Y'], [field_name_lon, 'CENTROID_X']], coordinate_system = coordinate_system)
